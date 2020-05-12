8945c79fdc0806ba77d80dca2418d7d0e9fb3ed4
Adam Barker <adam.barker@savant.com>
2020-05-12 15:57:31 -0400

    designtool api documentation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import pepe  from "./bigpepe.png"
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import pepe from "./bigpepe.png";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import styled from "styled-components";
+import Jobs from "./Endpoints/Jobs";
+
+const AppGrid = styled.div`
+  display: grid;
+  grid-template-columns: 20% 80%;
+  width: 100%;
+  height: 100%;
+`;
+
+const Sidebar = styled.div`
+  border-right: 1px solid #fafaf9;
+  padding: 20px;
+  height: 100%;
+`;
+
+const Content = styled.div`
+  padding: 40px;
+`;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Router>
+        <AppGrid>
+          <Sidebar>
+            <Link className="nav-link" to="/jobs">
+              Jobs
+            </Link>
+            <Link className="nav-link" to="/rooms">
+              Rooms
+            </Link>
+            <Link className="nav-link" to="/fixtures">
+              Fixtures
+            </Link>
+            <Link className="nav-link" to="/loads">
+              Loads
+            </Link>
+            <Link className="nav-link" to="/keypads">
+              Keypads
+            </Link>
+            <Link className="nav-link" to="/equipment">
+              Equipment
+            </Link>
+            <Link className="nav-link" to="/products">
+              Products
+            </Link>
+          </Sidebar>
+          <Content>
+            <Switch>
+              <Route path="/jobs">
+                <Jobs />
+              </Route>
+            </Switch>
+          </Content>
+        </AppGrid>
+      </Router>
+
+      {/* <header className="App-header">
         <img src={pepe} className="App-logo" alt="logo" />
         <a
           className="App-link"
@@ -16,7 +71,7 @@ function App() {
         >
           Damn grl u fine
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
